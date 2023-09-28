5759254d7c29af838c85d51299193b12788f1d26
Alexey <alexmail121212@gmail.com>
2023-09-28 17:43:36 +0300

    Task 10 done

--- EX_10/Program.cs
+++ EX_10/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+
+int num;
+
+Console.Clear();
+do
+{
+    Console.WriteLine("Введите трехзначное число (от 100 до 999 ):");
+    num = Convert.ToInt32(Console.ReadLine());
+} while ((num < 100) | (num > 999));
+
+Console.WriteLine();
+Console.WriteLine($"Вторая цифра числа {num} -> " + GetSecondDigit(num));
+
+// Methods
+
+int GetSecondDigit(int number)
+{
+    return (number % 100 - number % 10) / 10;
+}
